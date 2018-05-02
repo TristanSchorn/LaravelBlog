f07498c4d44c7e59ba53f84a15bbbb8b05f388a4
Tristan <tristan@vogcalgaryappdeveloper.com>
2018-05-01 22:33:56 -0600

    Fix for pagination bug, fix for post order.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,8 +5,8 @@
     @if(count($posts) > 0)
         @foreach($posts as $post)
             <div class="well">
-            <h3><a href="/posts/{{$post->id}}">{{$post->title}}</h3>
-            <small> Written on {{$post->created_at}} by {{@$post->user->name}}</small>
+                <h3><a href="/posts/{{$post->id}}">{{$post->title}}</h3>
+                <small> Written on {{$post->created_at}} by {{@$post->user->name}}</small>
             </div>
         @endforeach
         {{$posts->links()}}
